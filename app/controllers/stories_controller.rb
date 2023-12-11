583899fa6c5966260bd8fce6fe01e4810eab5681
VincentLin0619 <vincentlin19910619@gmail.com>
2023-12-11 17:36:04 +0800

    加上文章列表樣式

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,26 @@
 class StoriesController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @stories = current_user.stories.order(created_at: :asc)
+  end
+
   def new
     @story = current_user.stories.new
+  end
+
+  def create
+    @story = current_user.stories.new(story_params)
+    if @story.save
+      redirect_to stories_path, notice: '新增成功！！'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :content, :status)
   end
 end
